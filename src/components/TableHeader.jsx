c9642b204b16b39b73b7e87b48608394a20e09ee
henrique <hbaetaleite@gmail.com>
2020-07-09 19:06:48 -0300

    CodeClimate v2

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -4,8 +4,8 @@ import planetHeaders from '../services/data';
 const TableHeader = () => (
   <thead>
     <tr>
-      {planetHeaders.map((header, i) => (
-        <th key={i++}>{header}</th>
+      {planetHeaders.map((header) => (
+        <th key={header}>{header}</th>
       ))}
     </tr>
   </thead>
