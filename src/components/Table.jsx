056826ab50aa653b5595385806b14d8cabbc1e9b
henrique <hbaetaleite@gmail.com>
2020-07-09 17:24:22 -0300

    iniciando projeto!

---
+++ src/components/Table.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>dado 1</th>
+            <th>dado 2</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>Referencia dado 1</td>
+          <td>Referencia dado 2</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
