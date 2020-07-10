cbed4c1fc1df0f826e96a038b7b251fc4cd22621
henrique <hbaetaleite@gmail.com>
2020-07-10 16:56:59 -0300

    refatorando chamada da API para o App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import Table from './components/TableScreen/Table';
 import FilterBar from './components/FilterBar/FilterBar';
+import requestFetch from './actions';
 
-function App() {
-  return (
-    <div className="App">
-      <FilterBar />
-      <Table />
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    const { callAPI } = this.props;
+    callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <FilterBar />
+        <Table />
+      </div>
+    );
+  }
 }
+const mapDispatchToProps = (dispatch) => ({
+  callAPI: () => dispatch(requestFetch()),
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
--- src/components/TableScreen/Table.jsx
+++ src/components/TableScreen/Table.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import requestFetch from '../../actions';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 import Loading from './Loading';
 
 class Table extends Component {
-  componentDidMount() {
-    const { callAPI } = this.props;
-    callAPI();
-  }
   render() {
     const { loading } = this.props;
     if (loading) return <Loading />;
@@ -26,8 +21,4 @@ const mapStateToProps = (state) => ({
   loading: state.reducerAPI.isFetching,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  callAPI: () => dispatch(requestFetch()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Table);
+export default connect(mapStateToProps)(Table);
