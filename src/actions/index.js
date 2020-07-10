59fee3dc63e6725b87cb38e72e15030373e15897
Lourenço <loulage@gmail.com>
2020-07-10 16:23:40 -0300

    Created action and reducer for async api request

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import fetchSWAPI from '../service/apiSWAPI';
+
+export const REQUEST_API = 'REQUEST_API';
+export const REQUEST_API_SUCCESS = 'REQUEST_API_SUCCESS';
+
+export const requestApi = () => ({
+  type: REQUEST_API,
+  loading: true,
+});
+
+export const requestApiSuccess = (data) => ({
+  type: REQUEST_API_SUCCESS,
+  data,
+});
+
+export const getPlanets = (endpoint) => (dispatch) => {
+  dispatch(requestApi());
+  return fetchSWAPI(endpoint).then((data) => dispatch(requestApiSuccess(data)));
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,19 @@
-function emptyReducer() {
-  return {};
-}
+import { REQUEST_API, REQUEST_API_SUCCESS } from '../actions';
 
-export default emptyReducer;
+const initialState = {
+  data: [],
+  loading: false,
+};
+
+export const swapiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_API:
+      return { ...state, loading: action.loading };
+    case REQUEST_API_SUCCESS:
+      return { ...state, loading: action.loading, data: action.data };
+    default:
+      return state;
+  }
+};
+
+export default swapiReducer;
