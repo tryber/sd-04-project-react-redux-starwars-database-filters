062357b9bc62e737b278e1d3b34c3fada21f2a1a
henrique <hbaetaleite@gmail.com>
2020-07-10 19:45:08 -0300

    codeclimate v2

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import NameSearch from './NameSearch';
 import NumericSearch from './NumericSearch';
 
-const FilterBar = () => {
-  return (
-    <div>
-      <h3>Filter Planets</h3>
-      <NameSearch />
-      <NumericSearch />
-    </div>
-  );
-};
+const FilterBar = () => (
+  <div>
+    <h3>Filter Planets</h3>
+    <NameSearch />
+    <NumericSearch />
+  </div>
+);
 
 export default FilterBar;
