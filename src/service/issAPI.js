57273d65f903c54979de149a5debf17634bf4047
Victor L <victor.lustosacp@gmail.com>
2020-07-10 18:43:32 -0300

    CC

--- src/service/issAPI.js
+++ src/service/issAPI.js
@@ -1,6 +1,6 @@
 const STARS_BASE_API = 'https://swapi-trybe.herokuapp.com/api/';
 
-export const getCurrentStarsPlanet = (par) => 
+export const getCurrentStarsPlanet = (par) =>
   fetch(`${STARS_BASE_API}${par}`)
     .then((response) => response.json())
     .then((json) => json.results);
