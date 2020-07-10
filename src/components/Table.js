1f5f19c1cbc20c21e58e3d72a79541b5ec167288
igorgbr <igorgphotoarte@gmail.com>
2020-07-10 13:46:07 -0300

    igor - iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ src/components/Table.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Table = () => <div>StarWars Datatable with Filters</div>;
+
+export default Table;import React from 'react';
+
+const Table = () => <div>StarWars Datatable with Filters</div>;
+
+export default Table;
