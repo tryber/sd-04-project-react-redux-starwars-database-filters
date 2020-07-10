c6004def840037cfdba26ef55350ef795d52c551
matheusysd <matheusyurisd@gmail.com>
2020-07-10 01:52:33 -0300

    Só pra testar um negócio

---
+++ src/service/swApi.js
@@ -0,0 +1,8 @@
+const SW_BASE_API = 'http://swapi-trybe.herokuapp.com/api/planets';
+
+const getPLanets = () =>
+  fetch(SW_BASE_API).then((data) =>
+    data.json().then((json) => (data.ok ? Promise.resolve(json) : Promise.reject(json))),
+  );
+
+export default getPLanets;
