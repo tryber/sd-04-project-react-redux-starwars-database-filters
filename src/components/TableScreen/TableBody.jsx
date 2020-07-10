558f9ecbc8a132a8776824402774a3603a8738b4
henrique <hbaetaleite@gmail.com>
2020-07-10 17:06:59 -0300

    codeClimate v2

--- src/components/TableScreen/TableBody.jsx
+++ src/components/TableScreen/TableBody.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const TableBody = ({ planets }) => {
-  return (
-    <tbody>
-      {planets.map((planet) => (
-        <tr key={planet.name}>
-          <td>{planet.name}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.population}</td>
-          <td>{planet.films}</td>
-          <td>{planet.created}</td>
-          <td>{planet.edited}</td>
-          <td>{planet.url}</td>
-        </tr>
-      ))}
-    </tbody>
-  );
-};
+const TableBody = ({ planets }) => (
+  <tbody>
+    {planets.map((planet) => (
+      <tr key={planet.name}>
+        <td>{planet.name}</td>
+        <td>{planet.rotation_period}</td>
+        <td>{planet.orbital_period}</td>
+        <td>{planet.diameter}</td>
+        <td>{planet.climate}</td>
+        <td>{planet.gravity}</td>
+        <td>{planet.terrain}</td>
+        <td>{planet.surface_water}</td>
+        <td>{planet.population}</td>
+        <td>{planet.films}</td>
+        <td>{planet.created}</td>
+        <td>{planet.edited}</td>
+        <td>{planet.url}</td>
+      </tr>
+    ))}
+  </tbody>
+);
 
 const mapStateToProps = (state) => ({
   planets: state.reducerAPI.data,
