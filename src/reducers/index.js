35eb93bd2a4a53819c8499c466d265f81bdcf8d6
Diego Campos <diegodiko05@gmail.com>
2020-07-11 17:43:45 -0300

    Refatorando reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -34,63 +34,32 @@ const applyNumericFilters = (planets, filters) => {
 
 function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case REQUEST_PLANETS:
-      return {
-        ...state,
-      };
-    case REQUEST_PLANETS_SUCCESS:
+    case REQUEST_PLANETS: return { ...state }; case REQUEST_PLANETS_SUCCESS:
       return {
         ...state,
         planetsData: action.planetsData,
         filteredPlanets: action.planetsData,
         isFetching: false,
       };
-    case NAME_TO_FILTER:
-      return {
-        ...state,
-        filterByName: {
-          name: action.name,
-        },
-      };
-    case SET_FILTERED_BY_NAME:
-      return {
-        ...state,
-        filteredPlanets: action.planets,
-      };
-    case SET_FILTER_VARIABLES: {
-      return {
-        ...state,
-        filterByNumericValues: [
-          ...state.filterByNumericValues,
-          {
-            column: action.column,
-            comparison: action.comparison,
-            value: action.value,
-          },
-        ],
-      };
-    }
-    case SET_FILTERED_BY_NUMERIC: {
+    case NAME_TO_FILTER: return { ...state, filterByName: { name: action.name } };
+    case SET_FILTERED_BY_NAME: return { ...state, filteredPlanets: action.planets };
+    case SET_FILTER_VARIABLES: { return {
+      ...state,
+      filterByNumericValues: [
+        ...state.filterByNumericValues,
+        { column: action.column, comparison: action.comparison, value: action.value }],
+    };
+    } case SET_FILTERED_BY_NUMERIC: {
       const planets = state.filterByNumericValues.length === 0
         ? state.planetsData : state.filteredPlanets;
       const filteredPlanets = applyNumericFilters(planets, state.filterByNumericValues);
-      return {
-        ...state,
-        filteredPlanets,
-      };
-    }
-    case REMOVE_FILTER: {
+      return { ...state, filteredPlanets };
+    } case REMOVE_FILTER: {
       const newFilteredByNumericValues = state.filterByNumericValues.filter(
         ({ column }) => column !== action.filterToRemove.column,
       );
-
-      return {
-        ...state,
-        filterByNumericValues: newFilteredByNumericValues,
-      };
-    }
-    default:
-      return state;
+      return { ...state, filterByNumericValues: newFilteredByNumericValues };
+    } default: return state;
   }
 }
 
