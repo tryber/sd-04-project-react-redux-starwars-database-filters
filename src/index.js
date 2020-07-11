068fa6d236b989b2380ca5180315108c88762c11
juniomelos <juniomelos@gmail.com>
2020-07-10 21:53:35 -0300

    requirement 1 - CC

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Provider store={store}>
