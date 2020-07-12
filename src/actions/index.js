e37a7c147676675c3ec339ee48ecd916d6650984
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-07-12 19:28:44 -0300

    Bomba

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,2 @@
-import fetchSWAPI from '../services/fetchSWAPI';
-
-// API action
-export const REQUEST = 'REQUEST';
-export const requestSWAPI = () => ({
-  type: REQUEST,
-});
-
-export const RECEIVED_DATA = 'RECEIVED_DATA';
-export const receivedData = (data) => ({ type: RECEIVED_DATA, data });
-
-export const getSWAPI = (endpoint) => (dispatch) => {
-  dispatch(requestSWAPI());
-  return fetchSWAPI(endpoint).then((data) => dispatch(receivedData(data)));
-};
-//
-
-export const FILTER_BY_NAME = 'FILTER_BY_NAME';
-export const filterByName = (name) => ({ type: FILTER_BY_NAME, name });
+export { FILTER_BY_NAME, filterByName } from './filterByName';
+export { REQUEST, RECEIVED_DATA, getSWAPI } from './API';
