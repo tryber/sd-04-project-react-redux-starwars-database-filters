41ff502c67472c6a418415587c4920d439a884d6
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-07-12 13:05:07 -0300

    cc

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { requestFetch } from '../actions';
 import HeadTable from './HeadTable';
 import BodyTable from './BodyTable';
@@ -15,21 +15,20 @@ class Table extends React.Component {
   render() {
     const { isFetching, data } = this.props;
 
-    if(isFetching) return <h1>loading...</h1>
+    if (isFetching) return <h1>loading...</h1>
 
-    if(data) {
+    if (data) {
       return (
         <table>
           <thead>
             <HeadTable />
           </thead>
           <tbody>
-            {data.map((planet) => <BodyTable planet={planet} key={planet.name}/>)}
+            { data.map((planet) => <BodyTable planet={planet} key={planet.name}/>) }
           </tbody>
         </table>
-      )
+      );
     }
-  
   }
 }
 
@@ -41,5 +40,15 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   getPlanets: () => dispatch(requestFetch()),
 });
+
+Table.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object),
+  getPlanets: PropTypes.func.isRequired,
+};
+
+Table.defaultProps = {
+  data: null,
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
