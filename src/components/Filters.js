4d286782c5cca4d0565c80e61a280b645fa6ea27
matheusysd <matheusyurisd@gmail.com>
2020-07-12 03:00:58 -0300

    CC 2x0 EU

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -12,6 +12,12 @@ class Filters extends React.Component {
       comparison: '',
       value: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event;
+    this.setState({ [name]: value });
   }
 
   render() {
@@ -25,27 +31,17 @@ class Filters extends React.Component {
     return (
       <div>
         <form onSubmit={(e) => { e.preventDefault(); changeH(this.state); }}>
-          <select data-testid="column-filter"
-            onChange={(event) => this.setState({ column: event.target.value })}
-          >
-            {optionsN.map((option) => (
-              <option value={option}>{option}</option>
-            ))}
+          <select data-testid="column-filter" onChange={(event) => this.handleChange(event)}>
+            {optionsN.map((option) => (<option value={option}>{option}</option>))}
           </select>
-          <select data-testid="comparison-filter"
-            onChange={(event) => this.setState({ comparison: event.target.value })}
-          >
+          <select data-testid="comparison-filter" onChange={(event) => this.handleChange(event)}>
             <option defaultValue>Comparison</option>
             <option value="maior que">maior que</option>
             <option value="menor que">menor que</option>
             <option value="igual a">igual a</option>
           </select>
-          <input type="number" data-testid="value-filter"
-            onChange={(event) => this.setState({ value: event.target.value })}
-          />
-          <button type="submit" data-testid="button-filter">
-            Filtrar
-          </button>
+          <input type="number" data-testid="value-filter" onChange={(event) => this.handleChange(event)} />
+          <button type="submit" data-testid="button-filter">Filtrar</button>
         </form>
         <FiltersInUse />
       </div>
