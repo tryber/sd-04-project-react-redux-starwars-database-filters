1a1b1151f62c2c7781e4426a8df2e1790a56946a
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-13 15:41:51 -0300

    reducer que acessa dados do planeta

---
+++ src/reducers/planets.js
@@ -0,0 +1,29 @@
+import {
+  REQUEST_PLANETS,
+  REQUEST_PLANETS_SUCCESS
+} from '../actions';
+
+const INITIAL_PLANETS = {
+  isFetching: false,
+  data: {},
+};
+
+const planets = (state = INITIAL_PLANETS, action) => {
+  switch (action.type) {
+    case REQUEST_PLANETS:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case REQUEST_PLANETS_SUCCESS:
+      return {
+        ...state,
+        data: action.planets,
+        isFetching: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default planets;
