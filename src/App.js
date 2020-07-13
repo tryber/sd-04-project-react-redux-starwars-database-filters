1bc7f4f146ec225d75953262989bc65993f48560
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-07-13 19:31:13 -0300

    Initializing...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Table />
       </header>
     </div>
   );
