8190ecd34a541afbbfaacfe03cd8e33cd1dcec0c
Ronald Lima <ronaldazevedolima@gmail.com>
2020-07-13 16:12:44 -0300

    cc8

--- src/service/ApiRequest.js
+++ src/service/ApiRequest.js
@@ -2,7 +2,7 @@ const API_SW = 'http://swapi-trybe.herokuapp.com/api/planets';
 
 const apiSw = () => {
   return fetch(API_SW).then((data) =>
-    data.json().then((json) => (data.ok ? Promise.resolve(json) : Promise.reject(json)))
+    data.json().then((json) => (data.ok ? Promise.resolve(json) : Promise.reject(json))),
   );
 };
 
