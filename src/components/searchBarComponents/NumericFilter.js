3f6b399c62a14c7b2f890042a5248a49266dc31a
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-13 17:04:28 -0300

    tentando alegrar o cc

--- src/components/searchBarComponents/NumericFilter.js
+++ src/components/searchBarComponents/NumericFilter.js
@@ -16,7 +16,6 @@ class NumericFilter extends Component {
 
   handleClick(event) {
     event.preventDefault();
-    console.log(this.state)
     const { state } = this;
     this.props.numericFilter(state);
   }
@@ -24,7 +23,8 @@ class NumericFilter extends Component {
   render() {
     return (
       <form onSubmit={(e) => this.handleClick(e)}>
-        <select data-testid="column-filter" name="select" onChange={(e) => this.setState({ column: e.target.value })}>
+        <select data-testid="column-filter" name="select"
+          onChange={(e) => this.setState({ column: e.target.value })}>
           <option value="">Column</option>
           <option value="population">population</option>
           <option value="orbital_period">orbital_period</option>
@@ -32,13 +32,15 @@ class NumericFilter extends Component {
           <option value="rotation_period">rotation_period</option>
           <option value="surface_water">surface_water</option>
         </select>
-        <select data-testid="comparison-filter" name="select" onChange={(e) => this.setState({ comparison: e.target.value })}>
+        <select data-testid="comparison-filter" name="select"
+          onChange={(e) => this.setState({ comparison: e.target.value })}>
           <option value="">Comparison</option>
           <option value="less than">menor que</option>
           <option value="grather than">maior que</option>
           <option value="equal to">igual a</option>
         </select>
-        <input data-testid="value-filter" type="number" onChange={(e) => this.setState({ value: e.target.value })}/>
+        <input data-testid="value-filter" type="number"
+          onChange={(e) => this.setState({ value: e.target.value })} />
         <button type="submit" data-testid="button-filter">Filter</button>
       </form>
     );
