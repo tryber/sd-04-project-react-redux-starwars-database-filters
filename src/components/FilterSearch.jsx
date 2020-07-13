cac26519092d09f21b762cfab23284910ae482b3
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-07-13 20:46:22 -0300

    fix/codeclimate

--- src/components/FilterSearch.jsx
+++ src/components/FilterSearch.jsx
@@ -3,20 +3,18 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { search } from '../actions/filters';
 
-const FilterSearch = ({ searchName }) => {
-  return (
-    <label htmlFor="search">
-      Search
-      <input
-        type="text"
-        name="search"
-        id="search"
-        data-testid="name-filter"
-        onChange={(event) => searchName(event.target.value)}
-      />
-    </label>
-  );
-};
+const FilterSearch = ({ searchName }) => (
+  <label htmlFor="search">
+    Search
+    <input
+      type="text"
+      name="search"
+      id="search"
+      data-testid="name-filter"
+      onChange={(event) => searchName(event.target.value)}
+    />
+  </label>
+);
 
 const mapDispatchToProps = (dispatch) => ({
   searchName: (name) => dispatch(search(name)),
