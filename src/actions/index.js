2664a6afc56c3489dfb395dd01778da6912b39de
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-13 15:36:06 -0300

    action para pegar os dados do planeta

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import { getPlanetsData } from '../service/SWAPI';
+
+export const REQUEST_PLANETS = 'REQUEST_PLANETS';
+export const REQUEST_PLANETS_SUCCESS = 'REQUEST_SWAPI_PLANETS_SUCCESS';
+
+const requestPlanets = () => ({
+  type: REQUEST_PLANETS,
+});
+
+const requestPlanetsSuccess = (data) => ({
+  type: REQUEST_PLANETS_SUCCESS,
+  data,
+});
+
+export function fetchPlanets() {
+  return (dispatch) => {
+    dispatch(requestPlanets())
+
+    return getPlanetsData()
+      .then(
+        (planets) => dispatch(requestPlanetsSuccess(planets))
+      )
+  }
+}
