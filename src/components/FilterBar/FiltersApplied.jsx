7d83f3ee5d41109f89f9f16692107c247da11520
henrique <hbaetaleite@gmail.com>
2020-07-13 19:19:59 -0300

    codeclimate v2

--- src/components/FilterBar/FiltersApplied.jsx
+++ src/components/FilterBar/FiltersApplied.jsx
@@ -3,35 +3,33 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteFilter } from '../../actions/filter';
 
-const FiltersApplied = ({ filters, deleteFilter }) => {
-  return (
-    <div className="filters-applied">
-      {filters.length > 0 ? <h4>Filters applied</h4> : null}
-      {filters.map((filter) => (
-        <div className="filters-card" key={filter.column}>
-          <p>{filter.column}</p>
-          <p>{filter.comparison}</p>
-          <p>{filter.value}</p>
-          <button onClick={() => deleteFilter(filter.column)} className="remove-button">
-            remove filter
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-};
+const FiltersApplied = ({ filters, removeFilter }) => (
+  <div className="filters-applied">
+    {filters.length > 0 ? <h4>Filters applied</h4> : null}
+    {filters.map((filter) => (
+      <div className="filters-card" key={filter.column}>
+        <p>{filter.column}</p>
+        <p>{filter.comparison}</p>
+        <p>{filter.value}</p>
+        <button onClick={() => removeFilter(filter.column)} className="remove-button">
+          remove filter
+        </button>
+      </div>
+    ))}
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   filters: state.filters.filterByNumericValues,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  deleteFilter: (column) => dispatch(deleteFilter(column)),
+  removeFilter: (column) => dispatch(deleteFilter(column)),
 });
 
 FiltersApplied.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.object).isRequired,
-  deleteFilter: PropTypes.func.isRequired,
+  removeFilter: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FiltersApplied);
