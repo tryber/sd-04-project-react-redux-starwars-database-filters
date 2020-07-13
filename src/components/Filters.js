42df9d7fa19b49def6cf3ca91581062f70d844b3
deboracosilveira <deboracosilveira@gmail.com>
2020-07-13 19:30:57 -0300

    Requirement 5 - CC

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,21 +4,20 @@ import PropTypes from 'prop-types';
 
 import { deleteFilterByNumericValues } from '../actions';
 
-const Filters = ({ filtersInfo, deleteFilter }) => {
-  return (
-    <div>
-      <p>Filtros</p>
-      {filtersInfo.map((filter) => (
-        <div key={filter.column} data-testid="filter">
-          <button onClick={() => deleteFilter(filter.column)} type="button" >
-            X
-          </button>
-          <p>{`${filter.column} | ${filter.comparison} | ${filter.value}`}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+const Filters = ({ filtersInfo, deleteFilter }) => (
+  <div>
+    <p>Filtros</p>
+    {filtersInfo.map((filter) => (
+      <div key={filter.column} data-testid="filter">
+        <button onClick={() => deleteFilter(filter.column)} type="button" >
+          X
+        </button>
+        <p>{`${filter.column} | ${filter.comparison} | ${filter.value}`}</p>
+      </div>
+    ))}
+  </div>
+);
+
 
 const mapStateToProps = (state) => ({
   filtersInfo: state.filters.filterByNumericValues,
