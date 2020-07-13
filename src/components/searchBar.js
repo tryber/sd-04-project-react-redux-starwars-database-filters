7d62823544ddde40f83350b7994c792d7a508bbb
Ronald Lima <ronaldazevedolima@gmail.com>
2020-07-13 18:49:08 -0300

    re2-cc3

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,18 +1,18 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { inputName } from '../actions/index';
 
 const SearchBar = ({ inputName1 }) =>
-    <form>
-      <label htmlFor="inputTexto">Filtre por nome</label>
-      <input type="text" name="inputTexto" data-testid="name-filter" onChange={inputName1} />
-    </form>
-  ;
+  <form>
+    <label htmlFor="inputTexto">Filtre por nome</label>
+    <input type="text" name="inputTexto" data-testid="name-filter" onChange={inputName1} />
+  </form>
+;
 
 SearchBar.propTypes = {
   inputName1: PropTypes.func.isRequired,
-}
+};
 
 const mapDispatchToProps = (dispatch) => ({
   inputName1: (event) => dispatch(inputName(event)),
