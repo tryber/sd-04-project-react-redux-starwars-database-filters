12dfc16e9c57fa199d87311f0009daffde34c0d0
wistyk <leonardoxw@gmail.com>
2020-07-14 15:33:32 -0300

    hello, hello, hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from './components/table';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Table />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
