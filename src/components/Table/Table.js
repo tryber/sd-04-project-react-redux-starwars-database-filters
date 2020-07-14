b4a6f8c0d97cd2f6b0b1d30184b397e9307b6941
cacobribeiro <cacobribeiro@gmail.com>
2020-07-14 11:21:51 -0300

    Teste

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,37 +3,41 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Tabelas from './Tabelas';
 
-function Table({ isLoading, data }) {
+function Table({ isLoading, data, searchBar }) {
   if (isLoading) return <span>L O A D I N G . . .</span>;
   return (
     <div>
       <table>
         <thead>
           <tr>
-            {Object.keys(data.results[0]).map((e, i) => (
+            {Object.keys(data[0]).map((e, i) => (
               <th key={`${i + 1}`}>{e}</th>
             ))}
           </tr>
         </thead>
-        {data.results.map((planet, i) => (
-          <tbody key={i}>
+        {data.map((planet, i) => (
+          <tbody key={`${i + 1}`}>
             <Tabelas planet={planet} />
           </tbody>
         ))}
       </table>
+      <h1>{searchBar}</h1>
     </div>
   );
 }
 
 Table.propTypes = {
-  data: PropTypes.array.isRequired,
-  results: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    map: PropTypes.func,
+  }).isRequired,
   isLoading: PropTypes.bool.isRequired,
+  searchBar: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   isLoading: state.reducerFetch.isLoading,
-  data: state.reducerFetch.data,
+  data: state.reducerFetch.data.results,
+  searchBar: state.reducerInput.filters.filterByName.name,
 });
 
 export default connect(mapStateToProps)(Table);
