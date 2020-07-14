20d93f7b01d52b8498adffd520b473ceb110cdee
wistyk <leonardoxw@gmail.com>
2020-07-14 18:17:17 -0300

    requisitos para uso do redux montados

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,16 @@
-function emptyReducer() {
-  return {};
-}
+import { ACAO_A_SER_DESENVOLVIDA } from '../action';
 
-export default emptyReducer;
+const INITIAL_STATE = {
+  data: 0,
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ACAO_A_SER_DESENVOLVIDA:
+      return {
+
+      };
+    default:
+      return state;
+  }
+};
