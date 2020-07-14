3bdeaae6f5591c78c3b64443e862972e82f01b23
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-07-14 17:23:33 -0300

    commit pro Leandrão

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -86,7 +86,6 @@ class Table extends React.Component {
 
     const filteredSortedPlanets = this.sortPlanets(filteredPlanets);
 
-
     return (
       <tbody>
         {filteredSortedPlanets.map((planet) => (
