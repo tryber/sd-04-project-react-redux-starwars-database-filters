6331f22b9b048f55976dc24aea49b41737d7336e
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-07-15 01:39:53 -0300

    th

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -48,21 +48,5 @@ const mapStateToProps = (state) => ({
 export default connect(mapStateToProps)(TableBody);
 
 TableBody.protoTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      rotation_period: PropTypes.string,
-      orbital_period: PropTypes.string,
-      diameter: PropTypes.string,
-      climate: PropTypes.string,
-      gravity: PropTypes.string,
-      terrain: PropTypes.string,
-      surface_water: PropTypes.string,
-      population: PropTypes.string,
-      film: PropTypes.string,
-      created: PropTypes.string,
-      edited: PropTypes.string,
-      url: PropTypes.string,
-    }),
-  ).isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
