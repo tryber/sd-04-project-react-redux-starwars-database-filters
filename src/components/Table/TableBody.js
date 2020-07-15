0770f9e1fea0e86ffc1778317552e30e169ef7e0
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-07-15 00:57:22 -0300

    cc em tbody

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -2,44 +2,42 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { createStore } from 'redux';
 
-const TableBody = ({ data }) => {
-  return (
-    <tbody>
-      {data.map(
-        ({
-          name,
-          rotation_period: rotationPeriod,
-          orbital_period: orbitalPeriod,
-          diameter,
-          climate,
-          gravity,
-          terrain,
-          surface_water: surfaceWater,
-          population,
-          films,
-          created,
-          edited,
-        }) => (
-          <tr key={name}>
-            {name}
-            <td>{rotationPeriod}</td>
-            <td>{orbitalPeriod}</td>
-            <td>{diameter}</td>
-            <td>{climate}</td>
-            <td>{gravity}</td>
-            <td>{terrain}</td>
-            <td key={surfaceWater}>{surfaceWater}</td>
-            <td>{population}</td>
-            <td>{films}</td>
-            {/* lembrar de req. filmes para pegar o titulo */}
-            <td>{created}</td>
-            <td>{edited}</td>
-          </tr>
-        ),
-      )}
-    </tbody>
-  );
-};
+const TableBody = ({ data }) => (
+  <tbody>
+    {data.map(
+      ({
+        name,
+        rotation_period: rotationPeriod,
+        orbital_period: orbitalPeriod,
+        diameter,
+        climate,
+        gravity,
+        terrain,
+        surface_water: surfaceWater,
+        population,
+        films,
+        created,
+        edited,
+      }) => (
+        <tr key={name}>
+          {name}
+          <td>{rotationPeriod}</td>
+          <td>{orbitalPeriod}</td>
+          <td>{diameter}</td>
+          <td>{climate}</td>
+          <td>{gravity}</td>
+          <td>{terrain}</td>
+          <td key={surfaceWater}>{surfaceWater}</td>
+          <td>{population}</td>
+          <td>{films}</td>
+          {/* lembrar de req. filmes para pegar o titulo */}
+          <td>{created}</td>
+          <td>{edited}</td>
+        </tr>
+      ),
+    )}
+  </tbody>
+);
 
 const mapStateToProps = (state) => ({
   data: state.data,
