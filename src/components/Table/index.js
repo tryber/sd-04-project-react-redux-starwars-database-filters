d7e0f70538f3911492bff884070e6699b138c735
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-07-15 01:00:09 -0300

    teste cc em table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,16 +4,16 @@ import TableHead from './TableHead';
 import TableBody from './TableBody';
 // import { createStore } from 'redux';
 
-const Table = ({ fetching }) => {
-  return fetching ? (
+const Table = ({ fetching }) => (
+  fetching ? (
     <div>LOADING...</div>
   ) : (
     <table>
       <TableHead />
       <TableBody />
     </table>
-  );
-};
+  )
+);
 
 const mapStateToProps = (state) => ({
   // data: state.data,
