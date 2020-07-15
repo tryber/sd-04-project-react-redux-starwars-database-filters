41d72fce35117205e9d0cfbd1d6466b746d54b6c
Victor L <victor.lustosacp@gmail.com>
2020-07-15 16:16:04 -0300

    CC

--- src/components/ConteudoTable.js
+++ src/components/ConteudoTable.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const ConteudoTable = (data, title, input, comparison) => {
+const ConteudoTable = (data, title, input, comparisonMaster) => {
   let novo = data;
   if (input !== '') novo = data.filter((planet) => planet.name.toLowerCase().includes(input));
-  if (comparison.length > 0) {
-    comparison.forEach(({ column, comparison, value }) => {
+  if (comparisonMaster.length > 0) {
+    comparisonMaster.forEach(({ column, comparison, value }) => {
       novo = novo.filter((planet) => {
         if (comparison === 'maior que') return Number(planet[column]) > Number(value);
         if (comparison === 'menor que') return Number(planet[column]) < Number(value);
         if (comparison === 'igual a') return Number(planet[column]) === Number(value);
         return null;
       });
-      console.log(novo)
     },
   );
   }
--- src/components/FilterNumerico.js
+++ src/components/FilterNumerico.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { comparisonFilterAction } from '../actions/index';
 
 export class FilterNumerico extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      column: 'colunas',
-      comparison: 'comparaçao',
+      column: "colunas",
+      comparison: "comparaçao",
       value: 0,
     };
   }
@@ -15,22 +15,33 @@ export class FilterNumerico extends Component {
     const { column, comparison, value } = this.state;
     return (
       <div>
-        <select data-testid='column-filter' onChange={(e) => this.setState({ column: e.target.value })}>
-          <option value=''selected>colunas</option>
-          <option value='population'>population</option>
-          <option value='orbital_period'>orbital_period</option>
-          <option value='diameter'>diameter</option>
-          <option value='rotation_period'>rotation_period</option>
-          <option value='surface_water'>surface_water</option>
+        <select
+          data-testid="column-filter"
+          onChange={(e) => this.setState({ column: e.target.value })}
+        >
+          <option value="" selected>
+            colunas
+          </option>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
         </select>
-        <select data-testid='comparison-filter'  onChange={(e) => this.setState({ comparison: e.target.value })}>
-          <option value='' selected>comparação</option>
-          <option value='maior que'>maior que</option>
-          <option value='menor que'>menor que</option>
-          <option value='igual a'>igual a</option>
+        <select
+          data-testid="comparison-filter"
+          onChange={(e) => this.setState({ comparison: e.target.value })}
+        >
+          <option value="" selected>
+            comparação
+          </option>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
         </select>
         <input
-          type="number" data-testid="value-filter"
+          type="number"
+          data-testid="value-filter"
           id="victor"
           onChange={(e) => this.setState({ value: e.target.value })}
         />
@@ -38,14 +49,16 @@ export class FilterNumerico extends Component {
           type="button"
           data-testid="button-filter"
           onClick={() => this.props.comparison({ column, comparison, value })}
-        >Filtrar</button>
+        >
+          Filtrar
+        </button>
       </div>
-    )
+    );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  comparison: (Obj) => dispatch(comparisonFilterAction(Obj))
+  comparison: (Obj) => dispatch(comparisonFilterAction(Obj)),
 });
 
-export default connect(null, mapDispatchToProps)(FilterNumerico)
+export default connect(null, mapDispatchToProps)(FilterNumerico);
