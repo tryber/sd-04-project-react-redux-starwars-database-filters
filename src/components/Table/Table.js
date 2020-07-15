1c90b4acd9b24a2572ccae76aef9ce16ac9ff442
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-07-14 21:00:23 -0300

    Done

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -52,18 +52,18 @@ class Table extends React.Component {
     }, planets);
   }
 
-  sortPlanets(arr = [...this.props.data]) {
-    if (arr.length === 0) return arr;
+  sortPlanets(planets = [...this.props.data]) {
+    if (planets.length === 0) return planets;
     const { orderColumn, orderSort } = this.props;
     const planetKey = orderColumn.toLowerCase();
 
-    if (isNaN(arr[0][planetKey])) {
-      arr.sort((a, b) => (a[planetKey] > b[planetKey] ? 1 : -1));
+    if (isNaN(planets[0][planetKey])) {
+      planets.sort((a, b) => (a[planetKey] > b[planetKey] ? 1 : -1));
     } else {
-      arr.sort((a, b) => a[planetKey] - b[planetKey]);
+      planets.sort((a, b) => a[planetKey] - b[planetKey]);
     }
-    if (orderSort === 'DESC') arr.reverse();
-    return arr;
+    if (orderSort === 'DESC') planets.reverse();
+    return planets;
   }
 
   renderTableHead() {
