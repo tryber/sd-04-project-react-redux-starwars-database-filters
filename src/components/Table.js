dab29a9a86bc32a12f2883ba58ba38c39290850f
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-15 21:43:24 -0300

    Fix CCx

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,6 +9,26 @@ export class Table extends Component {
     getData("planets");
   }
 
+  tableHeader = () => {
+    return (
+      <tr>
+        <td>name</td>
+        <td>rotation_period</td>
+        <td>orbital_period</td>
+        <td>diameter</td>
+        <td>climate</td>
+        <td>gravity</td>
+        <td>terrain</td>
+        <td>surface_water</td>
+        <td>population</td>
+        <td>films</td>
+        <td>created</td>
+        <td>edited</td>
+        <td>url</td>
+      </tr>
+    );
+  };
+
   renderTable = () => {
     const { data } = this.props;
     return data.map((planet, i) => (
@@ -28,26 +48,6 @@ export class Table extends Component {
         <td>{planet.url}</td>
       </tr>
     ));
-  };
-
-  tableHeader = () => {
-    return (
-      <tr>
-        <td>name</td>
-        <td>rotation_period</td>
-        <td>orbital_period</td>
-        <td>diameter</td>
-        <td>climate</td>
-        <td>gravity</td>
-        <td>terrain</td>
-        <td>surface_water</td>
-        <td>population</td>
-        <td>films</td>
-        <td>created</td>
-        <td>edited</td>
-        <td>url</td>
-      </tr>
-    );
   };
 
   render() {
