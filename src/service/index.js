7dfeee504bc0aaf2ccd55fc1b04a310dc6db8d00
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-15 23:45:27 -0300

    kill me now

--- src/service/index.js
+++ src/service/index.js
@@ -10,10 +10,9 @@
 } */
 
 export default function apiRequest(endpoint) {
-  return fetch(`https://swapi-trybe.herokuapp.com/api/${endpoint}/`).then(
-    response =>
-      response.json().then(
-        // prettier-ignore
+  return fetch(`https://swapi-trybe.herokuapp.com/api/${endpoint}/`)
+    .then(response => response.json()
+      .then(
         data => response.ok ? Promise.resolve(data) : Promise.reject(data)
       )
   );
