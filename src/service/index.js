36a3ec0f4c2c33d75ea28bb76df9c512cb092ce1
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-15 21:52:59 -0300

    GEEEZUS MARY AND JOWSEF

--- src/service/index.js
+++ src/service/index.js
@@ -10,13 +10,11 @@
 } */
 
 export default function apiRequest(endpoint) {
-  return fetch(
-    `https://swapi-trybe.herokuapp.com/api/${endpoint}/`
-  ).then(response =>
-    response
-      .json()
-      .then(data =>
-        response.ok ? Promise.resolve(data) : Promise.reject(data)
+  return fetch(`https://swapi-trybe.herokuapp.com/api/${endpoint}/`).then(
+    response =>
+      response.json().then(
+        // prettier-ignore
+        data => response.ok ? Promise.resolve(data) : Promise.reject(data)
       )
   );
 }
