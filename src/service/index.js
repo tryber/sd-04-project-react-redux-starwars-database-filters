ff8f85022f4704368e4b451881354594081f564d
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-16 00:11:37 -0300

    :((((

--- src/service/index.js
+++ src/service/index.js
@@ -15,8 +15,8 @@ export default function apiRequest(endpoint) {
   ).then(response =>
     response
       .json()
-      .then(data =>
-        response.ok ? Promise.resolve(data) : Promise.reject(data)
+      .then(
+        data => response.ok ? Promise.resolve(data) : Promise.reject(data)
       )
   );
 }
