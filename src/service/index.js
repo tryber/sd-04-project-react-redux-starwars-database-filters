8e2aa300dce94528e80da2542c0a0d348917ea9a
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-16 00:28:04 -0300

    wish me luck

--- src/service/index.js
+++ src/service/index.js
@@ -12,11 +12,7 @@
 export default function apiRequest(endpoint) {
   return fetch(
     `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
-  ).then(response =>
-    response
-      .json()
-      .then(data =>
-        response.ok ? Promise.resolve(data) : Promise.reject(data),
-      ),
-  );
+  ).then((response) => response
+    .json()
+    .then((data) => (response.ok ? Promise.resolve(data) : Promise.reject(data))));
 }
