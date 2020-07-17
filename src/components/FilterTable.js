bd1e2b94948a3bf8d58e567edd4a8332e3e11eaa
Tiago-ers <trsilva284@gmail.com>
2020-07-16 23:16:04 -0300

    cc 2

--- src/components/FilterTable.js
+++ src/components/FilterTable.js
@@ -3,20 +3,16 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { filterTable } from '../action/actionFilter';
 
-const FilterTable = ({ filter }) => {
-  return (
-    <div>
-      <form htmlFor="name-filter">
-        <label>Filter by name:</label>
-        <input
-          data-testid="name-filter"
-          type="text"
-          onChange={(e) => filter(e.target.value)}
-        />
-      </form>
-    </div>
-  );
-};
+const FilterTable = ({ filter }) => (
+  <div>
+    <label>Filter by name:</label>
+    <input
+      data-testid="name-filter"
+      type="text"
+      onChange={(e) => filter(e.target.value)}
+    />
+  </div>
+);
 
 const mapDispatchToProps = (dispatch) => ({
   filter: (text) => dispatch(filterTable(text)),
