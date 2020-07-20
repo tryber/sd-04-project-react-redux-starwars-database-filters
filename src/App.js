9703f98be3972ab8675ac0c1dab83b9e0ae6aa60
Thiago Tavares <thiagooo15@gmail.com>
2020-07-20 14:00:04 -0300

    please

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Table from './components/Table/Table';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Table />
-      </header>
+      <Home />
     </div>
   );
 }
