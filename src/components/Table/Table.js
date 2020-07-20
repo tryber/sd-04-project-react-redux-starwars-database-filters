e341124ef425696be84587fb3c64ed76c6e90bba
cacobribeiro <cacobribeiro@gmail.com>
2020-07-20 13:19:37 -0300

    Erro no Filter

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,10 +3,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Tabelas from './Tabelas';
 
-function Table({ isLoading, data, searchBar }) {
+function Table({ isLoading, data, searchBar, filtros }) {
+  const allFilters = () => {
+    let planets = [];
+    if (filtros.length > 0) {
+      filtros.forEach(({ column, comparison, value }) => {
+        planets = planets.filter((planet) => {
+          if (comparison === 'menor') return Number(planet[column]) < Number(value);
+          if (comparison === 'igual') return Number(planet[column]) === Number(value);
+          if (comparison === 'maior') return Number(planet[column]) > Number(value);
+          return null;
+        });
+      });
+    }
+    planets = data.filter((e) => e.name.toLowerCase().includes(searchBar));
+    console.log(planets);
+    return planets;
+  };
+
   if (isLoading) return <span>L O A D I N G . . .</span>;
+  const planetsFiltred = allFilters();
   const offResidents = Object.keys(data[0]).filter((e) => e !== 'residents');
-  const filterByName = data.filter((e) => e.name.toLowerCase().includes(searchBar));
   return (
     <div>
       <table>
@@ -18,7 +35,7 @@ function Table({ isLoading, data, searchBar }) {
           </tr>
         </thead>
         <tbody>
-          {filterByName.map((planet, i) => (
+          {planetsFiltred.map((planet, i) => (
             <Tabelas planet={planet} i={i} />
           ))}
         </tbody>
@@ -44,6 +61,7 @@ const mapStateToProps = (state) => ({
   isLoading: state.reducerFetch.isLoading,
   data: state.reducerFetch.data.results,
   searchBar: state.filters.filterByName.name,
+  filtros: state.filters.filterByNumericValues,
 });
 
 export default connect(mapStateToProps)(Table);
