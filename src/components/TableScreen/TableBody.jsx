dacc97947f5c920c8a4ba0a4071ce023cfbc23ca
henrique <hbaetaleite@gmail.com>
2020-07-21 11:31:05 -0300

    codeclimate

--- src/components/TableScreen/TableBody.jsx
+++ src/components/TableScreen/TableBody.jsx
@@ -25,7 +25,8 @@ const TableBody = ({ planets, filter, filterNumeric }) => {
   if (filterNumeric.length > 0) {
     console.log('filterNumeric', filterNumeric);
     filterNumeric.forEach(
-      (filter) => (filteredPlanets = filteredPlanets.filter((planet) => compareFilters(planet, filter))),
+      (filter) =>
+        (filteredPlanets = filteredPlanets.filter((planet) => compareFilters(planet, filter))),
     );
   }
   const objKeys = filteredPlanets.length > 0 ? Object.keys(filteredPlanets[0]) : null;
