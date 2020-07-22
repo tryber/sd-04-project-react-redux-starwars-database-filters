283e7e0895a5ba233f37a277eda22ccce04805bf
Pedro Pires <pedropires@Pedros-MacBook-Air.local>
2020-07-21 21:24:23 -0300

    Inserting Redux Logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducer, composeWithDevTools(applyMiddleware(thunk, logger)),
+);
 
 export default store;
