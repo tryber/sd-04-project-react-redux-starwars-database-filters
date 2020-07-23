5dfd17af7b5801c9ae74243620553ffb53376dbb
djonata-dias <diasdjonata@gmail.com>
2020-07-23 14:05:32 -0300

    ajuste  no filtro numerico

--- src/components/NumberFilters.jsx
+++ src/components/NumberFilters.jsx
@@ -3,6 +3,27 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { filterByNumber } from '../actions/filters';
 
+const columnFilter = (optionsArray, testId) => (
+  <select data-testid={testId}>
+    {optionsArray.map(({ value, text }) => (<option key={value} value={value}>{text}</option>))}
+  </select>
+);
+
+const columnArray = [
+  { value: '', text: 'Column' },
+  { value: 'population', text: 'population' },
+  { value: 'orbital_period', text: 'orbital_period' },
+  { value: 'diameter', text: 'diameter' },
+  { value: 'rotation_period', text: 'rotation_period' },
+  { value: 'surface_water', text: 'surface_water' },
+];
+const comparisonArray = [
+  { value: '', text: 'Comparison' },
+  { value: 'maior que', text: 'maior que' },
+  { value: 'menor que', text: 'menor que' },
+  { value: 'igual a', text: 'igual a' },
+];
+
 const NumberFilters = ({ filterByNumberProps, filterByNumberState }) => (
   <form
     onSubmit={(e) => {
@@ -17,21 +38,8 @@ const NumberFilters = ({ filterByNumberProps, filterByNumberState }) => (
       filterByNumberProps(filterArray);
     }}
   >
-    <select data-testid="column-filter">
-      <option value="population">population</option>
-
-      <option value="population">population</option>
-      <option value="orbital_period">orbital_period</option>
-      <option value="diameter">diameter</option>
-      <option value="rotation_period">rotation_period</option>
-      <option value="surface_water">surface_water</option>
-    </select>
-    <select data-testid="comparison-filter">
-      <option value="population">population</option>
-      <option value="population">population</option>
-      <option value="menor que">menor que</option>
-      <option value="igual a">igual a</option>
-    </select>
+    {columnFilter(columnArray, 'column-filter')}
+    {columnFilter(comparisonArray, 'comparison-filter')}
     <input data-testid="value-filter" type="number" />
     <button data-testid="button-filter" type="submit">Apply filters</button>
   </form>
