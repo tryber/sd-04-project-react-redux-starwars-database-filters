5be73b2f9ac685e8a523b7e81452195062300f25
djonata-dias <diasdjonata@gmail.com>
2020-07-23 13:33:23 -0300

    test

--- src/components/NumberFilters.jsx
+++ src/components/NumberFilters.jsx
@@ -19,13 +19,16 @@ const NumberFilters = ({ filterByNumberProps, filterByNumberState }) => (
   >
     <select data-testid="column-filter">
       <option value="population">population</option>
+
+      <option value="population">population</option>
       <option value="orbital_period">orbital_period</option>
       <option value="diameter">diameter</option>
       <option value="rotation_period">rotation_period</option>
       <option value="surface_water">surface_water</option>
     </select>
     <select data-testid="comparison-filter">
-      <option value="maior que">maior que</option>
+      <option value="population">population</option>
+      <option value="population">population</option>
       <option value="menor que">menor que</option>
       <option value="igual a">igual a</option>
     </select>
