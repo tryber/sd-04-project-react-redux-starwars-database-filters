35811bd647c5ca0c7c9e40aafb2ac2c796eecb21
Thabata <dornelasthabata@gmail.com>
2020-07-25 16:08:54 -0300

    CC

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -13,27 +13,27 @@ const filters = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FILTER_BY_NAME:
       return {
-          ...state,
-          filterByName: { name: action.name },
-        };
+        ...state,
+        filterByName: { name: action.name },
+      };
     case FILTER_BY_NUMERIC_VALUES:
       return {
-          ...state,
-          filterByNumericValues: [...state.filterByNumericValues, {
+        ...state,
+        filterByNumericValues: [...state.filterByNumericValues, {
             column: action.column,
             comparison: action.comparison,
             value: action.value,
           }],
-        };
+      };
     case REMOVE_FILTER_BY_NUMERIC_VALUES:
       return {
-          ...state,
-          filterByNumericValues: [
+        ...state,
+        filterByNumericValues: [
             ...state.filterByNumericValues.filter(
               (filter) => filter !== action.filterKeys,
             ),
           ],
-        };
+      };
     default:
       return state;
   }
