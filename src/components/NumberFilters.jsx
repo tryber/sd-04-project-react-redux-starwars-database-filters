09508d28d1d4b31483b4a54073e62fdaa5d3df2c
djonata-dias <diasdjonata@gmail.com>
2020-07-25 04:55:50 -0300

    ajuste no slect de filter number

--- src/components/NumberFilters.jsx
+++ src/components/NumberFilters.jsx
@@ -11,14 +11,14 @@ const columnFilter = (optionsArray, testId, id) => (
   </select>
 );
 
-const columnArray = [
-  { value: '', text: 'Column' },
-  { value: 'population', text: 'population' },
-  { value: 'orbital_period', text: 'orbital_period' },
-  { value: 'diameter', text: 'diameter' },
-  { value: 'rotation_period', text: 'rotation_period' },
-  { value: 'surface_water', text: 'surface_water' },
-];
+// const columnArray = [
+//   { value: '', text: 'Column' },
+//   { value: 'population', text: 'population' },
+//   { value: 'orbital_period', text: 'orbital_period' },
+//   { value: 'diameter', text: 'diameter' },
+//   { value: 'rotation_period', text: 'rotation_period' },
+//   { value: 'surface_water', text: 'surface_water' },
+// ];
 const comparisonArray = [
   { value: '', text: 'Comparison' },
   { value: 'maior que', text: 'maior que' },
@@ -37,17 +37,17 @@ const NumberFilters = ({ filterByNumberProps, filterByNumberState }) => (
       };
       const columnOptions = document.querySelectorAll('#column-option');
       columnOptions.forEach((option) => {
-        if (option.value === e.target.children[0].value) {
+        if (option.value === e.target.children[0].value && option.value !== 'Column') {
           option.remove();
         }
       });
       console.log(columnOptions);
       const filterNumber = filterByNumberState;
-      if (filterByNumberState.length === 0) {
+      if (filterByNumberState.length === 0 && e.target.children[0].value !== 'Column') {
         filterNumber.push(inputValues);
         filterByNumberProps(filterNumber);
       } else {
-        if (!filterNumber.find((filter) => filter.column === inputValues.column)) {
+        if (!filterNumber.find((filter) => filter.column === inputValues.column) && e.target.children[0].value !== 'Column') {
           filterNumber.push(inputValues);
         }
         const newFilter = filterNumber.map((filter) => {
@@ -60,7 +60,15 @@ const NumberFilters = ({ filterByNumberProps, filterByNumberState }) => (
       }
     }}
   >
-    {columnFilter(columnArray, 'column-filter', 'column-option')}
+    {/* {columnFilter(columnArray, 'column-filter', 'column-option')} */}
+    <select data-testid="column">
+      <option id="column-option" value="Column">Column</option>
+      <option id="column-option" value="population">population</option>
+      <option id="column-option" value="orbital_period">orbital_period</option>
+      <option id="column-option" value="diameter">diameter</option>
+      <option id="column-option" value="rotation_period">rotation_period</option>
+      <option id="column-option" value="surface_water">surface_water</option>
+    </select>
     {columnFilter(comparisonArray, 'comparison-filter', 'comparison-option')}
     <input data-testid="value-filter" type="number" />
     <button data-testid="button-filter" type="submit">Apply filters</button>
