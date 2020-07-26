0e63dcf96b0a3b52603f0ac0560984f6b1b26227
wistyk <leonardoxw@gmail.com>
2020-07-26 16:04:04 -0300

    provider passado para appjs na funçao, verificar no plantao

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
-    </div>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
 }
 
