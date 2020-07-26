b3f0f22a12707a848ac7761ce7b08a84d1eec93c
Thabata <dornelasthabata@gmail.com>
2020-07-26 18:18:36 -0300

    CC

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,49 +6,49 @@ import RenderTable from './renderTable';
 import { compare, sortData } from '../usingSort';
 
 class Table extends Component {
-orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
-  const objKeys = planetsData.length !== 0 ? Object.keys(planetsData[0]).filter((keys) => keys !== 'residents') : [];
-    let planets = planetsData.sort((a, b) => a.name.localeCompare(b.name));
-    if (order.column !== 'Name') {
-      planets = sortData(planets, order);
-    }
-    if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
-      filterByNumericValues.forEach((filter) => {
-        const { column, comparison, value } = filter;
-        planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
-      });
-    }
-    if (searchText !== '') {
-      planets = planets.filter(
-        (planet) =>
-          planet.name.includes(searchText.toUpperCase()) ||
-          planet.name.includes(searchText.toLowerCase()),
+  orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
+    const objKeys = planetsData.length !== 0 ? Object.keys(planetsData[0]).filter((keys) => keys !== 'residents') : [];
+      let planets = planetsData.sort((a, b) => a.name.localeCompare(b.name));
+      if (order.column !== 'Name') {
+        planets = sortData(planets, order);
+      }
+      if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
+        filterByNumericValues.forEach((filter) => {
+          const { column, comparison, value } = filter;
+          planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
+        });
+      }
+      if (searchText !== '') {
+        planets = planets.filter(
+          (planet) =>
+            planet.name.includes(searchText.toUpperCase()) ||
+            planet.name.includes(searchText.toLowerCase()),
+        );
+      }
+      return (
+        <table className="table">
+          <Table objKeys={objKeys} planets={planets} />
+        </table>
       );
-    }
-    return (
-      <table className="table">
-        <Table objKeys={objKeys} planets={planets} />
-      </table>
-    );
-  };
+   };
 
-  componentDidMount() {
-    const { filteredPlanets: fetch } = this.props;
-    fetch();
-  }
+componentDidMount() {
+  const { filteredPlanets: fetch } = this.props;
+  fetch();
+};
 
-  test() {
-    const { filteredPlanets } = this.props;
-    return filteredPlanets;
-  }
+test() {
+  const { filteredPlanets } = this.props;
+  return filteredPlanets;
+};
 
-  render() {
-    const { planetsData, filteredPlanets, isFetching } = this.props;
-    if (isFetching) return <p>Loading...</p>;
-    const headerTitles = planetsData ? Object.keys(planetsData[0]) : [];
-    return <RenderTable tableHeaderTitles={headerTitles} filteredPlanets={filteredPlanets} />;
+render() {
+  const { planetsData, filteredPlanets, isFetching } = this.props;
+  if (isFetching) return <p>Loading...</p>;
+  const headerTitles = planetsData ? Object.keys(planetsData[0]) : [];
+  return <RenderTable tableHeaderTitles={headerTitles} filteredPlanets={filteredPlanets} />;
   }
-}
+};
 
 const mapStateToProps = (state) => ({
   planetsData: state.filters.planetsData,
