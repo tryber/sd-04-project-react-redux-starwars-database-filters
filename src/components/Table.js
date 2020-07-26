f083fb87d6fb53d6fb87c55c961b1611380f4738
Thabata <dornelasthabata@gmail.com>
2020-07-26 18:28:30 -0300

    T____T

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,34 +3,34 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import fetchPlanets from '../actions/fetchPlanets';
 import RenderTable from './renderTable';
-import { compare, sortData } from '../usingSort';
+// import { compare, sortData } from '../usingSort';
 
 class Table extends Component {
-  orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
-    const objKeys = planetsData.length !== 0 ? Object.keys(planetsData[0]).filter((keys) => keys !== 'residents') : [];
-      let planets = planetsData.sort((a, b) => a.name.localeCompare(b.name));
-      if (order.column !== 'Name') {
-        planets = sortData(planets, order);
-      }
-      if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
-        filterByNumericValues.forEach((filter) => {
-          const { column, comparison, value } = filter;
-          planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
-        });
-      }
-      if (searchText !== '') {
-        planets = planets.filter(
-          (planet) =>
-            planet.name.includes(searchText.toUpperCase()) ||
-            planet.name.includes(searchText.toLowerCase()),
-        );
-      }
-      return (
-        <table className="table">
-          <Table objKeys={objKeys} planets={planets} />
-        </table>
-      );
-   };
+  // orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
+  //   const objKeys = planetsData.length !== 0 ? Object.keys(planetsData[0]).filter((keys) => keys !== 'residents') : [];
+  //     let planets = planetsData.sort((a, b) => a.name.localeCompare(b.name));
+  //     if (order.column !== 'Name') {
+  //       planets = sortData(planets, order);
+  //     }
+  //     if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
+  //       filterByNumericValues.forEach((filter) => {
+  //         const { column, comparison, value } = filter;
+  //         planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
+  //       });
+  //     }
+  //     if (searchText !== '') {
+  //       planets = planets.filter(
+  //         (planet) =>
+  //           planet.name.includes(searchText.toUpperCase()) ||
+  //           planet.name.includes(searchText.toLowerCase()),
+  //       );
+  //     }
+  //     return (
+  //       <table className="table">
+  //         <Table objKeys={objKeys} planets={planets} />
+  //       </table>
+  //     );
+  //  };
 
 componentDidMount() {
   const { filteredPlanets: fetch } = this.props;
