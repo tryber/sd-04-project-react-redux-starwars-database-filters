190cfe1ccc04db0965ed008bdb78e2e467ea02f3
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-07-26 17:38:10 -0300

    req 5

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,4 +1,8 @@
-import { FILTER_BY_NAME, FILTER_BY_NUMERIC_VALUE, REMOVE_FILTERS } from '../actions/dataAction';
+import {
+  FILTER_BY_NAME,
+  FILTER_BY_NUMERIC_VALUE,
+  REMOVE_FILTERS,
+} from "../actions/dataAction";
 
 const INITIAL_STATE = {
   filterByName: {
@@ -17,18 +21,24 @@ function filters(state = INITIAL_STATE, action) {
     case FILTER_BY_NUMERIC_VALUE:
       return {
         ...state,
-        filterByNumericValues: [...state.filterByNumericValues, {
-          column: action.column,
-          comparison: action.comparison,
-          value: action.value,
-        }],
+        filterByNumericValues: [
+          ...state.filterByNumericValues,
+          {
+            column: action.column,
+            comparison: action.comparison,
+            value: action.value,
+          },
+        ],
       };
     case REMOVE_FILTERS:
       return {
-        ...state.filterByNumericValues.filter(
-          (key) => key !== action.filteredKeys
-        ),
-      }
+        ...state,
+        filterByNumericValues: [
+          ...state.filterByNumericValues.filter(
+            (key) => key !== action.filteredKeys
+          ),
+        ],
+      };
     default:
       return state;
   }
