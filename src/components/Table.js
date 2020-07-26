8a4fd7c753305234b5b33e4f4363316e33b7ab61
Thabata <dornelasthabata@gmail.com>
2020-07-26 18:31:17 -0300

    T__T

--- src/components/Table.js
+++ src/components/Table.js
@@ -32,23 +32,23 @@ class Table extends Component {
   //     );
   //  };
 
-componentDidMount() {
-  const { filteredPlanets: fetch } = this.props;
-  fetch();
-};
-
-test() {
-  const { filteredPlanets } = this.props;
-  return filteredPlanets;
-};
-
-render() {
-  const { planetsData, filteredPlanets, isFetching } = this.props;
-  if (isFetching) return <p>Loading...</p>;
-  const headerTitles = planetsData ? Object.keys(planetsData[0]) : [];
-  return <RenderTable tableHeaderTitles={headerTitles} filteredPlanets={filteredPlanets} />;
+  componentDidMount() {
+    const { filteredPlanets: fetch } = this.props;
+    fetch();
   }
-};
+
+  test() {
+    const { filteredPlanets } = this.props;
+    return filteredPlanets;
+  }
+
+  render() {
+    const { planetsData, filteredPlanets, isFetching } = this.props;
+    if (isFetching) return <p>Loading...</p>;
+    const headerTitles = planetsData ? Object.keys(planetsData[0]) : [];
+    return <RenderTable tableHeaderTitles={headerTitles} filteredPlanets={filteredPlanets} />;
+    }
+  }
 
 const mapStateToProps = (state) => ({
   planetsData: state.filters.planetsData,
@@ -66,7 +66,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(Table);
 
 Table.propTypes = {
   planetsData: PropTypes.arrayOf(PropTypes.object).isRequired,
-  fetchPlanets: PropTypes.func.isRequired,
+  // fetchPlanets: PropTypes.func.isRequired,
   isFetching: PropTypes.bool.isRequired,
   filteredPlanets: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
