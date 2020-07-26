319a1242085fd2e6db796bddcdc7a2f7068fcc95
Thabata <dornelasthabata@gmail.com>
2020-07-26 17:27:34 -0300

    CC

--- src/actions/filterByOrder.js
+++ src/actions/filterByOrder.js
@@ -1,9 +1,9 @@
+export const sortFilter = () => ({ type: SORT_FILTER });
+
 export function sortedFilter() {
   const sortFilter = (sort, column) => ({
     type: SORT_FILTER,
     sort,
     column,
   }
-);
-
-export const sortFilter = () => ({ type: SORT_FILTER })};
+)};
