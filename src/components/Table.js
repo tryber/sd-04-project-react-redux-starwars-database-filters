10034b96eba89b7e4975c5d35478d3334840eb35
Thabata <dornelasthabata@gmail.com>
2020-07-26 18:10:04 -0300

    Teste

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,36 +5,35 @@ import fetchPlanets from '../actions/fetchPlanets';
 import RenderTable from './renderTable';
 import { compare, sortData } from '../usingSort';
 
-const orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
-  const objKeys =
-    data.length !== 0 ? Object.keys(data[0]).filter((keys) => keys !== 'residents') : [];
-  let planets = data.sort((a, b) => a.name.localeCompare(b.name));
-  if (order.column !== 'Name') {
-    planets = sortData(planets, order);
-  }
-  if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
-    filterByNumericValues.forEach((filter) => {
-      const { column, comparison, value } = filter;
-      planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
-    });
-  }
-  if (searchText !== '') {
-    planets = planets.filter(
-      (planet) =>
-        planet.name.includes(searchText.toUpperCase()) ||
-        planet.name.includes(searchText.toLowerCase()),
-    );
-  }
-  return (
-    <table className="table">
-      <Table objKeys={objKeys} planets={planets} />
-    </table>
-  );
-};
-
 class Table extends Component {
+orderingPlanets = ({ planetsData, searchText, filterByNumericValues, order }) => {
+  const objKeys = planetsData.length !== 0 ? Object.keys(planetsData[0]).filter((keys) => keys !== 'residents') : [];
+    let planets = planetsData.sort((a, b) => a.name.localeCompare(b.name));
+    if (order.column !== 'Name') {
+      planets = sortData(planets, order);
+    }
+    if (typeof filterByNumericValues && filterByNumericValues.length > 0) {
+      filterByNumericValues.forEach((filter) => {
+        const { column, comparison, value } = filter;
+        planets = planets.filter((planeta) => compare(planeta, column, comparison, value));
+      });
+    }
+    if (searchText !== '') {
+      planets = planets.filter(
+        (planet) =>
+          planet.name.includes(searchText.toUpperCase()) ||
+          planet.name.includes(searchText.toLowerCase()),
+      );
+    }
+    return (
+      <table className="table">
+        <Table objKeys={objKeys} planets={planets} />
+      </table>
+    );
+  };
+
   componentDidMount() {
-    const { orderingPlanets: fetch } = this.props;
+    const { filteredPlanets: fetch } = this.props;
     fetch();
   }
 
