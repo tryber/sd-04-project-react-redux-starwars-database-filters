2a04a438a6950937ce3e0d74a03e1c2f0c423806
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-27 03:38:11 -0300

    planet request add

---
+++ src/action/index.js
@@ -0,0 +1,29 @@
+import apiPlanets from '../services/index';
+
+export const REQUEST_PLANETS = 'REQUEST_PLANETS';
+export const REQUEST_PLANETS_SUCCESS = 'REQUEST_PLANETS_SUCCESS';
+export const REQUEST_PLANETS_FAILURE = 'REQUEST_PLANETS_FAILURE';
+
+const requestPlanets = () => ({
+  type: REQUEST_PLANETS,
+});
+
+const sucessPlanets = (data) => ({
+  type: REQUEST_PLANETS_SUCCESS,
+  data,
+});
+
+const failurePlanets = (error) => ({
+  type: REQUEST_PLANETS_FAILURE,
+  error,
+});
+
+export function requestFetch() {
+  return (dispatch) => {
+    dispatch(requestPlanets());
+
+    return apiPlanets().then((json) => dispatch(sucessPlanets(json.results)),
+    (error) => dispatch(failurePlanets(error)),
+    );
+  };
+}
---
+++ src/reducers/getPlanets.js
@@ -0,0 +1,13 @@
+
+const INITIAL_STATE = {
+  data: [],
+}
+
+const getPlanets = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case value:
+  
+    default:
+      return state;
+  }
+}
