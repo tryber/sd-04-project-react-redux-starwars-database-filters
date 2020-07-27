24a8fcab1d45ecb8ed22e96ee91915d664e352b6
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-27 16:35:03 -0300

    Requisito 01 okay

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,10 @@
-const uri = 'https://swapi-trybe.herokuapp.com/api/planets/';
+const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
-const apiPlanets = () => fetch(uri)
+const apiPlanets = () => fetch(url)
   .then((response) => response.json()
     .then((json) => (
       response.ok ? Promise.resolve(json) : Promise.reject(json)
     )),
-  );
+);
 
 export default apiPlanets;
