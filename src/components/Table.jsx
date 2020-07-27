5a09bd0d9b8827cfaff05ea9c2c06e1bb782f2f5
Julio Cesar <stininmatic@hotmail.com>
2020-07-27 02:23:41 -0300

    Entendendo a parada

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,32 +4,29 @@ import { connect } from 'react-redux';
 import './Table.css';
 import linhas from './Linhas';
 
-class Table extends Component {
-  render() {
-    const { data, isFetching, name } = this.props;
-    const filterName = data.filter((planet) => planet.name.includes(name));
+const Table = (props) => {
+  const { data, isFetching, name } = props;
+  const filterName = data.filter((planet) => planet.name.includes(name));
 
-    if (isFetching) return <span>...Loading</span>;
-    const planeta1 = data;
-    const colunas = Object.keys(planeta1[0]).filter((campo) => campo !== 'residents');
-    console.log(name);
-    return (
-      <div>
-        <table className="tabela">
-          <thead>
-            <tr>
-              {colunas.map((coluna) => (
-                <th key={coluna}>{coluna}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>{filterName.map((chosenPlanet) => linhas(chosenPlanet))}</tbody>
-
-        </table>
-      </div>
-    );
-  }
-}
+  if (isFetching) return <span>...Loading</span>;
+  const planeta1 = data;
+  const colunas = Object.keys(planeta1[0]).filter((campo) => campo !== 'residents');
+  console.log(name);
+  return (
+    <div>
+      <table className="tabela">
+        <thead>
+          <tr>
+            {colunas.map((coluna) => (
+              <th key={coluna}>{coluna}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{filterName.map((chosenPlanet) => linhas(chosenPlanet))}</tbody>
+      </table>
+    </div>
+  );
+};
 
 Table.propTypes = {
   data: PropTypes.shape({
