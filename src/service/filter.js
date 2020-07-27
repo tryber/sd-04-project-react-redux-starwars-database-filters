5d9a828ed8e42d16edd157dd3309ab04a6f01183
Tiago-ers <trsilva284@gmail.com>
2020-07-27 16:57:25 -0300

    cc

--- src/service/filter.js
+++ src/service/filter.js
@@ -3,25 +3,25 @@ function filter(data, inputText, filterByNumericValues) {
 
   if (inputText) {
     filterData = data.filter((planeta) =>
-      planeta.name.toLowerCase().includes(inputText)
+      planeta.name.toLowerCase().includes(inputText),
     );
   }
   if (filterByNumericValues.length !== 0) {
     filterByNumericValues.forEach((itens) => {
       if (itens.comparison === 'maior que') {
         filterData = filterData.filter(
-          (item) => Number(item[itens.column]) > Number([itens.value])
+          (item) => Number(item[itens.column]) > Number([itens.value]),
         );
         // console.log(filterData, itens.comparison);
       }
       if (itens.comparison === 'igual a') {
         filterData = filterData.filter(
-          (item) => Number(item[itens.column]) === Number([itens.value])
+          (item) => Number(item[itens.column]) === Number([itens.value]),
         );
       }
       if (itens.comparison === 'menor que') {
         filterData = filterData.filter(
-          (item) => Number(item[itens.column]) < Number([itens.value])
+          (item) => Number(item[itens.column]) < Number([itens.value]),
         );
       }
     });
