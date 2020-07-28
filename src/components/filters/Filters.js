d75096fff8cd56bd3d6206c810ff810848ef9d10
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-28 19:07:27 -0300

    Filtros added

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import FilterName from './FilterByName';
+import FilterValues from './FiltersValue';
+import FilterOrder from './FilterOrder';
+import RemoveFilter from './RemoveFilter';
 
 function Filters() {
   return (
     <div>
       <FilterName />
+      <FilterValues />
+      <FilterOrder />
+      <RemoveFilter />
     </div>
   );
 }
