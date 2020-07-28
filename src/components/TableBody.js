5fd9d0debe1ff6dfb066e5268364379221fefd7d
wistyk <leonardoxw@gmail.com>
2020-07-28 16:55:10 -0300

    caminho absoluto na funçao

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import filtersFunc from './function';
+import filtersFunc from '/home/leonhard/Documentos/Trybe/sd-04-project-react-redux-starwars-database-filters/src/components/function';
 
 class TableBody extends Component {
   render() {
