04bebce92a40acc9a55f9477cc92b403a27c4060
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 19:08:51 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Table from './components/Table';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Table />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Home />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
