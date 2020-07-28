5477736cd0b4fc1c1722571fef364eff18e10b5c
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-28 19:09:38 -0300

    actions - API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
-import { getPlanetsData } from '../service/SWAPI';
+import getPlanetsData from '../service/SWAPI';
 
 export const REQUEST_PLANETS = 'REQUEST_PLANETS';
 export const REQUEST_PLANETS_SUCCESS = 'REQUEST_SWAPI_PLANETS_SUCCESS';
+export const REQUEST_PLANETS_FAIL = 'REQUEST_PLANETS_FAIL';
 
 const requestPlanets = () => ({
   type: REQUEST_PLANETS,
@@ -12,13 +13,17 @@ const requestPlanetsSuccess = (data) => ({
   data,
 });
 
+const requestPlanetsFail = () => ({
+  type: REQUEST_PLANETS_FAIL,
+});
+
 export function fetchPlanets() {
   return (dispatch) => {
     dispatch(requestPlanets());
 
-    return getPlanetsData()
-      .then(
-        (planets) => dispatch(requestPlanetsSuccess(planets))
-      );
+    return getPlanetsData().then(
+      (planets) => dispatch(requestPlanetsSuccess(planets.results)),
+      (error) => dispatch(requestPlanetsFail(error)),
+    );
   };
 }
