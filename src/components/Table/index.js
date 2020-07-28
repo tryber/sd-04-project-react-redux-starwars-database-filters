e6b600eb00e0386fcd10668ccaa0af950ee100ee
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-07-28 17:59:30 -0300

    aaa

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import TableHead from './TableHead';
 import TableBody from './TableBody';
 
-const Table = ({ fetching }) => {
-  return fetching ? (
-    <div>LOADING...</div>
-  ) : (
-    <table>
-      <TableHead />
-      <TableBody />
-    </table>
-  );
-};
+class Table extends Component {
+  render() {
+    const { fetching } = this.props;
+    return fetching ? (
+      <div>LOADING...</div>
+    ) : (
+      <table>
+        <TableHead />
+        <TableBody />
+      </table>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   // data: state.data,
