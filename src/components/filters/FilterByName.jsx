01ad360f6b8b64477562abadb619e8f89fbd9c47
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-28 16:48:58 -0300

    CC 04

--- src/components/filters/FilterByName.jsx
+++ src/components/filters/FilterByName.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { filterByName } from '../../action';
 
-function FilterByName({ filterByName }) {
-  return (
-    <div>
-      <input
-        type="text"
-        data-testid="name-filter"
-        placeholder="Pesquise um planeta"
-        onChange={(event) => filterByName(event.target.value)}
-      />
-    </div>
-  );
+class FilterByName extends Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          data-testid="name-filter"
+          placeholder="Pesquise um planeta"
+          onChange={(event) => this.props.filterByName(event.target.value)}
+        />
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
