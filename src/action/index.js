6324d6435b64b70f460120d467fa8e198ee98f04
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-07-28 14:59:50 -0300

    reducer filter filterBynumericValues

--- src/action/index.js
+++ src/action/index.js
@@ -5,6 +5,8 @@ export const REQUEST_PLANETS_SUCCESS = 'REQUEST_PLANETS_SUCCESS';
 export const REQUEST_PLANETS_FAILURE = 'REQUEST_PLANETS_FAILURE';
 export const FILTER_BY_NAME = 'FILTER_BY_NAME';
 export const ORDER_COLUMN = 'ORDER_COLUMN';
+export const FILTER_BY_NUMERIC_VALUES = 'FILTER_BY_NUMERIC_VALUES';
+export const REMOVE_FILTER_BY_NUMERIC_VALUES = 'REMOVE_FILTER_BY_NUMERIC_VALUES';
 
 const requestPlanets = () => ({
   type: REQUEST_PLANETS,
@@ -39,4 +41,16 @@ export const orderColumns = (column, sort) => ({
   type: ORDER_COLUMN,
   column,
   sort,
+});
+
+export const filterByNumericValues = (column, comparison, value) => ({
+  type: FILTER_BY_NUMERIC_VALUES,
+  column,
+  comparison,
+  value,
+});
+
+export const removeFilterNumeric = (obj) => ({
+  type: REMOVE_FILTER_BY_NUMERIC_VALUES,
+  obj,
 });
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,7 +1,13 @@
-import { FILTER_BY_NAME, ORDER_COLUMN } from '../action/index';
+import {
+  FILTER_BY_NAME,
+  ORDER_COLUMN,
+  FILTER_BY_NUMERIC_VALUES,
+  REMOVE_FILTER_BY_NUMERIC_VALUES,
+} from '../action/index';
 
 const INITIAL_STATE = {
   filterByName: { name: '' },
+  filterByNumericValues: [],
   order: {
     column: 'Name',
     sort: 'ASC',
@@ -12,12 +18,28 @@ const filters = (state = INITIAL_STATE, action) => {
   console.log(state)
   switch (action.type) {
     case FILTER_BY_NAME:
-      return { ...state, filterByName: { name: action.name } }
+      return { ...state, filterByName: { name: action.name } };
     case ORDER_COLUMN:
-      return { ...state, order: { column: action.column, sort: action.sort } }
+      return { ...state, order: { column: action.column, sort: action.sort } };
+    case FILTER_BY_NUMERIC_VALUES:
+      return {
+        ...state,
+        filterByNumericValues: [...state.filterByNumericValues,
+        {
+          column: action.column,
+          comparison: action.comparison,
+          value: action.value,
+        }],
+      };
+    case REMOVE_FILTER_BY_NUMERIC_VALUES:
+      return {
+        ...state,
+        filterByNumericValues: [
+          ...state.filterByNumericValues.filter((filter) => filter !== action.obj),
+        ] };
     default:
       return state;
   }
-}
+};
 
 export default filters;
