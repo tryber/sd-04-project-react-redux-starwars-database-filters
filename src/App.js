7611350f9cdf7b859d143a5b1c40da5f3a430bfb
igorgbr <igorgphotoarte@gmail.com>
2020-07-28 17:24:14 -0300

    request API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Table from './components/Table';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home />
         <Table />
       </header>
     </div>
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import getPlanetsAPI from '../services/getPlanetsAPI';
+
+getPlanetsAPI().then((data) => console.log(data.results));
+
+export class Home extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Home;
