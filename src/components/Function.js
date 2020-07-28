ccc1c50fce7f07d465ba1bbcc37d3439566339e9
wistyk <leonardoxw@gmail.com>
2020-07-28 17:08:59 -0300

    solucao

---
+++ src/components/Function.js
@@ -0,0 +1,30 @@
+const FiltersFunc = (planets, name, numericValues) => (
+  (
+    numericValues.length === 0
+      ? planets.filter((planet) => planet.name.includes(name))
+      : numericValues.reduce(
+        (acc, { column, comparison, value }) => acc.filter((planet) => {
+          switch (comparison) {
+            case 'maior que':
+              return (
+                planet.name.includes(name)
+                  && planet[column] > value
+              );
+            case 'menor que':
+              return (
+                planet.name.includes(name)
+                  && planet[column] < value
+              );
+            case 'igual a':
+              return (
+                planet.name.includes(name)
+                  && planet[column] === value
+              );
+            default:
+              return planet.name.includes(name);
+          }
+        }),
+        planets,
+      )));
+
+export default FiltersFunc;
