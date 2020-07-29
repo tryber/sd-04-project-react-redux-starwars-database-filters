78b5c2674fd41fed59986de0dbb9199c9df7abde
binhus <eng.robsonwilli@gmail.com>
2020-07-29 19:41:27 -0300

    CC issues

--- src/reducers/listaReducers.js
+++ src/reducers/listaReducers.js
@@ -12,25 +12,13 @@ function filters(state = INITIAL_STATE, action) {
     case 'REQUEST_API':
       return { ...state, isFetching: true };
     case 'SUCCESS':
-      return {
-        ...state,
-        isFetching: false,
-        data: action.data.results,
-      };
+      return { ...state, isFetching: false, data: action.data.results };
     case 'CHANGE_TEXT_INPUT':
-      return {
-        ...state,
-        filterByName: {
-          name: action.value,
-        },
-      };
+      return { ...state, filterByName: { name: action.value } };
     case 'CHANGE_TEXT_INPUT_COMPARISOR':
       return {
         ...state,
-        filterByNumericValues: {
-          ...state.filterByNumericValues,
-          [action.name]: action.value,
-        },
+        filterByNumericValues: { ...state.filterByNumericValues, [action.name]: action.value },
       };
     case 'COMPARISOR':
       return {
