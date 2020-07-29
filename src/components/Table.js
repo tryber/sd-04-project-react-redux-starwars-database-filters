9ff96e781d4e4f1494ad5f90ede7c9d88d1e775f
Josias Viveiro <josiasviveiro@gmail.com>
2020-07-29 16:16:10 -0300

    Requisito 1

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,28 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
-/* const Table = ({isFetching, data}) => {
-  return (
-    <div>
-      StarWars Datatable with Filters
-      {console.log(data)}
-      {console.log(isFetching)}
-      {console.log(data)}
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-}; */
+/*
+consy { data } = this.props;
+const keys = data.length >= 1 ? Object.keys(data[0]) : [];
+const tableHeader = keys.filter((key) => key !== 'residents');
+
+const getHeaders = (planets) => Object.keys(planets[0]).filter((header) => header !== 'residents');
+ */
 
 class Table extends React.Component {
   render() {
@@ -41,6 +26,16 @@ class Table extends React.Component {
               ))}
             </tr>
           </thead>
+
+          <tbody>
+            {data.map((planet) => (
+              <tr key={planet.name}>
+                {tableHeader.map((column) => (
+                  <td key={planet[column]}>{planet[column]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
@@ -48,17 +43,9 @@ class Table extends React.Component {
 }
 
 Table.propTypes = {
-  data: PropTypes.shape({
-    length: PropTypes.number,
-  }).isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-/*
-consy { data } = this.props;
-const keys = data.length >= 1 ? Object.keys(data[0]) : [];
-const tableHeader = keys.filter((key) => key !== 'residents');
 
-const getHeaders = (planets) => Object.keys(planets[0]).filter((header) => header !== 'residents');
- */
 const mapStateToProps = (state) => ({
   data: state.planets.data,
 });
