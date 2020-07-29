1ab600f8375626e1e66ce350a29b4621957dfe23
Julio Cesar <stininmatic@hotmail.com>
2020-07-29 19:30:56 -0300

    Requisito 4

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-// import './Table.css';
 import linhas from './Linhas';
 import filterAll from './filterAll';
 
