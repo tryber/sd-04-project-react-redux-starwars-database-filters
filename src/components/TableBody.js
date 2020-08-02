5efe8ce1e36872183eea65677fc96328578877ac
wistyk <leonardoxw@gmail.com>
2020-08-02 17:12:30 -0300

    refatorando

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -3,65 +3,66 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import FiltersFunc from './Function';
 
+const dataOrganization = (planets, name, numericValues, order) => {
+  const data = FiltersFunc(planets, name, numericValues, order);
+  const sorter = data.sort((actual, before) => {
+    let asc = '';
+    switch (order.column) {
+      case 'name':
+        asc = before.name.localeCompare(actual.name);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'rotation_period':
+        asc = actual.rotation_period - before.rotation_period;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'orbital_period':
+        asc = actual.orbital_period - before.orbital_period;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'diameter':
+        asc = actual.diameter - before.diameter;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'climate':
+        asc = actual.climate.localeCompare(before.climate);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'gravity':
+        asc = actual.gravity.localeCompare(before.gravity);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'terrain':
+        asc = actual.terrain.localeCompare(before.terrain);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'surface_water':
+        asc = actual.surface_water - before.surface_water;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'population':
+        asc = actual.population - before.population;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'residents':
+        asc = actual.residents.localeCompare(before.residents);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'films':
+        asc = actual.films.localeCompare(before.films);
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'created':
+        asc = actual.created - before.created;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      case 'edited':
+        asc = actual.edited - before.edited;
+        return order.sort === 'ASC' ? asc : asc * -1;
+      default:
+        asc = actual.name.localeCompare(before.name);
+        return order.sort === 'ASC' ? asc : asc * -1;
+    }
+  });
+  return sorter;
+};
+
 class TableBody extends Component {
   render() {
     const {
       planets, name, numericValues, order,
     } = this.props;
-    const dataOrganization = () => {
-      const data = FiltersFunc(planets, name, numericValues, order);
-      const sorter = data.sort((actual, before) => {
-        let asc = '';
-        switch (order.column) {
-          case 'name':
-            asc = before.name.localeCompare(actual.name);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'rotation_period':
-            asc = actual.rotation_period - before.rotation_period;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'orbital_period':
-            asc = actual.orbital_period - before.orbital_period;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'diameter':
-            asc = actual.diameter - before.diameter;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'climate':
-            asc = actual.climate.localeCompare(before.climate);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'gravity':
-            asc = actual.gravity.localeCompare(before.gravity);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'terrain':
-            asc = actual.terrain.localeCompare(before.terrain);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'surface_water':
-            asc = actual.surface_water - before.surface_water;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'population':
-            asc = actual.population - before.population;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'residents':
-            asc = actual.residents.localeCompare(before.residents);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'films':
-            asc = actual.films.localeCompare(before.films);
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'created':
-            asc = actual.created - before.created;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          case 'edited':
-            asc = actual.edited - before.edited;
-            return order.sort === 'ASC' ? asc : asc * -1;
-          default:
-            asc = actual.name.localeCompare(before.name);
-            return order.sort === 'ASC' ? asc : asc * -1;
-        }
-      });
-      return sorter;
-    };
     return (
       <tbody>
-        {dataOrganization().map((planet) => (
+        {dataOrganization(planets, name, numericValues, order).map((planet) => (
           <tr key={planet.name}>
             <td>{planet.name}</td>
             <td>{planet.rotation_period}</td>
