9169a802ccda018bcdaa2c9d1b66191b7094aa99
Tiago-ers <trsilva284@gmail.com>
2020-08-03 15:53:13 -0300

    cc8

--- src/components/PlanetsOptions.js.js
+++ src/components/PlanetsOptions.js.js
@@ -2,16 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import options from '../service/planets';
 
-const PlanetsOptions = ({ onChange }) => {
-  return (
-    <select name="column" data-testid="column-sort" onChange={onChange}>
-      <option>Ordem</option>
-      {options.map((opt) => (
-        <option>{opt}</option>
-      ))}
-    </select>
-  );
-};
+const PlanetsOptions = ({ onChange }) => (
+  <select name="column" data-testid="column-sort" onChange={onChange}>
+    <option>Ordem</option>
+    {options.map((opt) => (
+      <option>{opt}</option>
+    ))}
+  </select>
+);
 
 PlanetsOptions.propTypes = {
   onChange: PropTypes.func.isRequired,
--- src/components/PlanetsRadio.js
+++ src/components/PlanetsRadio.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PlanetsRadio = ({ onClick }) => {  
-  return (
-    <div>
-      <label htmlFor="orderASC">
-        <input
-          type="radio"
-          name="sort"
-          value="ASC"
-          test="column-sort-input"
-          onClick={onClick}
-        />
-        ASC
-      </label>
-      <label htmlFor="orderDSC">
-        <input
-          type="radio"
-          value="DESC"
-          name="sort"
-          test="column-sort-input"
-          onClick={onClick}
-        />
-        DESC
-      </label>
-    </div>
-  );
-};
+const PlanetsRadio = ({ onClick }) => (
+  <div>
+    <label htmlFor='orderASC'>
+      <input
+        type='radio'
+        name='sort'
+        value='ASC'
+        test='column-sort-input'
+        onClick={onClick}
+      />
+      ASC
+    </label>
+    <label htmlFor='orderDSC'>
+      <input
+        type='radio'
+        value='DESC'
+        name='sort'
+        test='column-sort-input'
+        onClick={onClick}
+      />
+      DESC
+    </label>
+  </div>
+);
 
 PlanetsRadio.propTypes = {
   onClick: PropTypes.func.isRequired,
