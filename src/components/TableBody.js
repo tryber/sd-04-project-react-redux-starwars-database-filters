564a4b21219fcbb7a18a11d963122122d7e1710e
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-08-04 14:52:50 -0300

    Create TableBody.js

---
+++ src/components/TableBody.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function TableBody({ planets }) {
+  return (
+    <tbody>
+      {planets.map((planet) => (
+        <tr key={planet.name}>
+          <td>{planet.name}</td>
+          <td>{planet.rotation_period}</td>
+          <td>{planet.orbital_period}</td>
+          <td>{planet.diameter}</td>
+          <td>{planet.climate}</td>
+          <td>{planet.gravity}</td>
+          <td>{planet.terrain}</td>
+          <td>{planet.surface_water}</td>
+          <td>{planet.population}</td>
+          <td>
+            {planet.films.map((film) => (
+              <span key={film}>{film}</span>
+            ))}
+          </td>
+          <td>{planet.created}</td>
+          <td>{planet.edited}</td>
+          <td>{planet.url}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  planets: state.getPlanets.data,
+});
+
+export default connect(mapStateToProps)(TableBody);
+
+TableBody.defaultProps = {
+  columnSort: 'Name',
+  sort: 'ASC',
+};
+
+TableBody.propTypes = {
+  planets: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      rotation_period: PropTypes.string,
+      orbital_period: PropTypes.string,
+      diameter: PropTypes.string,
+      climate: PropTypes.string,
+      gravity: PropTypes.string,
+      terrain: PropTypes.string,
+      surface_water: PropTypes.string,
+      population: PropTypes.string,
+      film: PropTypes.string,
+      created: PropTypes.string,
+      edited: PropTypes.string,
+      url: PropTypes.string,
+    }),
+  ).isRequired,
+};
