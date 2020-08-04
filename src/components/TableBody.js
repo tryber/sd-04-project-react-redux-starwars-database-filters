aed55b7e5d84f324583761cf9195875764b56157
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-08-04 12:56:17 -0300

    Table Body - *store problem

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import filterFunc from '../services/filterFunc';
-
 
 class TableBody extends Component {
   render() {
@@ -36,45 +34,9 @@ class TableBody extends Component {
   }
 }
 
-const mapState = (state) => ({
+const mapStateToProps = (state) => ({
   planets: state.getPlanets.data,
-  name: state.filters.filterByName.name,
-  numericValues: state.filters.filterByNumericValues,
 });
 
-export default connect(mapState)(TableBody);
+export default connect(mapStateToProps)(TableBody);|
 
-TableBody.defaultProps = {
-  columnSort: 'Name',
-  sort: 'ASC',
-};
-
-TableBody.propTypes = {
-  planets: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      rotation_period: PropTypes.string,
-      orbital_period: PropTypes.string,
-      diameter: PropTypes.string,
-      climate: PropTypes.string,
-      gravity: PropTypes.string,
-      terrain: PropTypes.string,
-      surface_water: PropTypes.string,
-      population: PropTypes.string,
-      film: PropTypes.string,
-      created: PropTypes.string,
-      edited: PropTypes.string,
-      url: PropTypes.string,
-    }),
-  ).isRequired,
-  name: PropTypes.string.isRequired,
-  numericValues: PropTypes.arrayOf(
-    PropTypes.shape({
-      column: PropTypes.string,
-      comparison: PropTypes.string,
-      value: PropTypes.string,
-      columnSort: PropTypes.string,
-      sort: PropTypes.string,
-    }),
-  ).isRequired,
-};
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const composeEnhancers =
-  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
