75d4dc2ce9ddfe64cb7ac84dfb061907f874c020
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-08-04 14:23:51 -0300

    Reducers, Actions, Srore and Home

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPlanets } from '../action';
+
+class Home extends Component {
+  componentDidMount() {
+    const { getPlanets } = this.props;
+    getPlanets();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>StarWars DataTable</h2>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  loading: state.getPlanets.isFetching,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getPlanets: () => dispatch(fetchPlanets())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/reducers/getPlanets.js
@@ -0,0 +1,29 @@
+import { REQUEST_PLANETS, REQUEST_PLANETS_SUCCESS, REQUEST_PLANETS_FAILURE } from '../action';
+
+const INITIAL_STATE = {
+  isFetching: false,
+  data: [],
+};
+
+const getPlanets = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case REQUEST_PLANETS:
+      return { ...state, isFetching: true };
+    case REQUEST_PLANETS_SUCCESS:
+      return {
+        ...state,
+        data: [...action.data],
+        isFetching: false,
+      };
+    case REQUEST_PLANETS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export default getPlanets;
