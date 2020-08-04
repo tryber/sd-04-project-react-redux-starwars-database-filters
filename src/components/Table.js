14c9a7bc64208ab9b85ee181fb6fb9b30936ea3b
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-08-04 07:49:43 -0300

    worked on table data and component proptypes

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,7 +6,19 @@ function Table() {
     <table>
       <thead>
         <tr>
-          <th>header</th>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Film</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>Url</th>
         </tr>
       </thead>
       <TableBody />
