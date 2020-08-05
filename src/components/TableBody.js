bf6ca6cbac0110926370b2900d2c8d67004d4798
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-08-05 11:24:54 -0300

    codeclimate fix

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -8,26 +8,26 @@ function TableBody({ planets, name, numericValues }) {
   return (
     <tbody>
       {data.map((planet) => (
-      <tr key={planet.name}>
-        <td>{planet.name}</td>
-        <td>{planet.rotation_period}</td>
-        <td>{planet.orbital_period}</td>
-        <td>{planet.diameter}</td>
-        <td>{planet.climate}</td>
-        <td>{planet.gravity}</td>
-        <td>{planet.terrain}</td>
-        <td>{planet.surface_water}</td>
-        <td>{planet.population}</td>
-        <td>
-          {planet.films.map((film) => (
-            <span key={film}>{film}</span>
-          ))}
-        </td>
-        <td>{planet.created}</td>
-        <td>{planet.edited}</td>
-        <td>{planet.url}</td>
-      </tr>
-    ))}
+        <tr key={planet.name}>
+          <td>{planet.name}</td>
+          <td>{planet.rotation_period}</td>
+          <td>{planet.orbital_period}</td>
+          <td>{planet.diameter}</td>
+          <td>{planet.climate}</td>
+          <td>{planet.gravity}</td>
+          <td>{planet.terrain}</td>
+          <td>{planet.surface_water}</td>
+          <td>{planet.population}</td>
+          <td>
+            {planet.films.map((film) => (
+              <span key={film}>{film}</span>
+            ))}
+          </td>
+          <td>{planet.created}</td>
+          <td>{planet.edited}</td>
+          <td>{planet.url}</td>
+        </tr>
+      ))}
     </tbody>
   );
 }
